60e21be98a0371cfb1a2926595e3e397abd2ef74
kosenkova-j <kojulev@gmail.com>
2025-01-16 01:29:59 +0700

    подключение ключа flickr

--- app/src/main/java/com/example/lab7/FlickrFetchr.kt
+++ app/src/main/java/com/example/lab7/FlickrFetchr.kt
@@ -14,12 +14,12 @@ private const val TAG = "FlickrFetchr"
 class FlickrFetchr {
     private val flickrApi: FlickrApi
     init {
-        val retrofit: Retrofit = Retrofit.Builder().baseUrl("https://www.flickr.com/").addConverterFactory(ScalarsConverterFactory.create()).build()
+        val retrofit: Retrofit = Retrofit.Builder().baseUrl("https://api.flickr.com/").addConverterFactory(ScalarsConverterFactory.create()).build()
         flickrApi = retrofit.create(FlickrApi::class.java)
     }
-    fun fetchContents(): LiveData<String> {
+    fun fetchPhotos(): LiveData<String> {
         val responseLiveData: MutableLiveData<String> = MutableLiveData()
-        val flickrRequest: Call<String> = flickrApi.fetchContents()
+        val flickrRequest: Call<String> = flickrApi.fetchPhotos()
         flickrRequest.enqueue(object : Callback<String> {
             override fun onFailure(call: Call<String>, t: Throwable) {
                 Log.e(TAG, "Failed to fetch photos", t)
--- app/src/main/java/com/example/lab7/api/FlickrApi.kt
+++ app/src/main/java/com/example/lab7/api/FlickrApi.kt
@@ -4,6 +4,6 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface FlickrApi {
-    @GET("/")
-    fun fetchContents(): Call<String>
+    @GET("services/rest/?method=flickr.interestingness.getList" + "&api_key=4dc061d5b27f7eb1eb4cbe851f541e1e" + "&format=json" + "&nojsoncallback=1" + "&extras=url_s")
+    fun fetchPhotos(): Call<String>
 }
